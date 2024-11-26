72d0bccbcc75c5d90a90dc555f2d67bdab00f028
Zane Zook <zookz@ohsu.edu>
2024-11-26 10:47:59 -0800

    Adjustment to auto advance freesurfer

--- Components/Freesurfer/scripts/importdata_recon-all.sh
+++ Components/Freesurfer/scripts/importdata_recon-all.sh
@@ -3,6 +3,6 @@
 export FREESURFER_HOME=$1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export FS_LOAD_DWI=0
-recon-all -sd "$2" -s "$3" -i "$4"
+echo -ne "\n" | recon-all -sd "$2" -s "$3" -i "$4"
 chmod -R +rwx "$2"
-recon-all -cw256 -sd "$2" -s "$3" -all
+echo -ne "\n" | recon-all -cw256 -sd "$2" -s "$3" -all
