24af04654bbb3bd525f086a64a647d5e065456bf
Zane Zook <zookz@ohsu.edu>
2026-01-22 15:48:12 -0800

    radomized selected model

--- launch_viewer.m
+++ launch_viewer.m
@@ -6,22 +6,27 @@ if isempty(projects)
     disp('No projects selected.')
 end
 
-%if numel(projects) > 1
-    subjectDataMap = containers.Map();
-    surfaceModel = [];
-    for i=1:numel(projects)
-        proj = projects{i};
-        if isempty(surfaceModel)
-            [subj,surfaceModel] = loadSubj(~isscalar(projects), proj);
-        else
-            [subj,~] = loadSubj(true, proj);
-        end
-        [~,subjName] = fileparts(proj);
-        subjectDataMap(subjName) = subj;
+subjectDataMap = containers.Map();
+surfaceModel = [];
+
+n_proj = numel(projects);
+surf_idx = randi(n_proj);
+
+for i=1:n_proj
+    proj = projects{i};
+    if i == surf_idx
+        % This subject provides the surface
+        [subj, surfaceModel] = loadSubj(~isscalar(projects), proj);
+    else
+        % All others load without surface
+        [subj, ~] = loadSubj(true, proj);
     end
-    
-    % === Launch the viewer ===
-    viewer = Super3DView(surfaceModel,subjectDataMap);
+    [~,subjName] = fileparts(proj);
+    subjectDataMap(subjName) = subj;
+end
+
+% === Launch the viewer ===
+viewer = Super3DView(surfaceModel,subjectDataMap);
 %else
 %    [subj,surface] = loadSubj(false, projects{1});
 
@@ -29,23 +34,23 @@ end
 %end
 
 function [subj,surface]=loadSubj(mni,proj)
-    dir = "DataOutput/";
-    if mni
-        dir = dir + "MNIbrain.mat";
-    else
-        dir = dir + "brain.mat";
-    end
-    data = fullfile(proj,dir);
-    load(data,'surfaceModel');
-    surface = surfaceModel;
+dir = "DataOutput/";
+if mni
+    dir = dir + "MNIbrain.mat";
+else
+    dir = dir + "brain.mat";
+end
+data = fullfile(proj,dir);
+load(data,'surfaceModel');
+surface = surfaceModel;
 
-    load(data,'electrodes');    
-    % format data to make VERA happy
-    elDef.Definition = electrodes.Definition;
-    elDef.Name = 'ElectrodeDefinition';
-    
-    elPos = rmfield(electrodes,'Definition');
+load(data,'electrodes');
+% format data to make VERA happy
+elDef.Definition = electrodes.Definition;
+elDef.Name = 'ElectrodeDefinition';
 
-    subj.ElectrodePositions = elPos;
-    subj.ElectrodeDefinitions = elDef;
+elPos = rmfield(electrodes,'Definition');
+
+subj.ElectrodePositions = elPos;
+subj.ElectrodeDefinitions = elDef;
 end
